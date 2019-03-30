1989c4b78b2da5bf06663c6bc469e7909b711505
Magesh <tkmagesh77@gmail.com>
2019-03-30 17:36:53 +0530

    Day-01 complete

---
+++ bug-tracker/src/app/bugTracker/bugTracker.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { Bug } from './models/Bug';
+import { BugOperationsService } from './services/bugOperations.service';
+
+@Component({
+	selector : 'app-bug-tracker',
+	templateUrl : 'bugTracker.component.html'
+})
+export class BugTrackerComponent{
+	bugs : Bug[] = [];
+
+	constructor(private bugOperations : BugOperationsService){
+
+	}
+	onAddNewClick(bugName : string){
+		let newBug = this.bugOperations.createNew(bugName);
+		this.bugs.push(newBug);
+	}
+
+	onBugNameClick(bug : Bug){
+		this.bugOperations.toggle(bug);
+	}
+
+	onRemoveClosedClick(){
+		this.bugs = this.bugs.filter(bug => !bug.isClosed);
+	}
+
+	
+}
